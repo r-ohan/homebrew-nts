cd8cfe2fc3a4cb737507430a8a8e72a36a98fede
Rohan Shah <rohans@myupfront.com>
2026-06-22 17:05:30 -0500

    nts 0.1.3

--- Formula/nts.rb
+++ Formula/nts.rb
@@ -1,8 +1,8 @@
 class Nts < Formula
   desc "Rich terminal home for NTS Radio"
   homepage "https://github.com/r-ohan/nts-radio-cli"
-  url "https://github.com/r-ohan/nts-radio-cli/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "38674257f1c158969e16620321dc8e0245a6902ad14aac6d82f04dcebd295b87"
+  url "https://github.com/r-ohan/nts-radio-cli/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "743ec4863486f7126ef630cf7c96e94c480a19f09e30dc397e541a1adad48406"
   license "MIT"
 
   depends_on "rust" => :build
